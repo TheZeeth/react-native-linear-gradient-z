fd678cb002800e73ee11622645dc390448ac253e
Miguelo Atla <migueloatla@gmail.com>
2023-06-05 19:01:54 +0200

    web support added

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-import { Platform } from "react-native";
-import LinearGradientIos from "./index.ios.js";
-import LinearGradientAndroid from "./index.android.js";
-import LinearGradientWindows from "./index.windows.js";
+import LinearGradient from "."
 
-const LinearGradient = Platform.OS === "ios"
-  ? LinearGradientIos : Platform.OS === "android"
-  ? LinearGradientAndroid : LinearGradientWindows;
-
-export default LinearGradient;
+export default LinearGradient
---
+++ index.web.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+import { View } from "react-native"
+
+class LinearGradient extends React.PureComponent {
+  render() {
+    return <View {...this.props} />
+  }
+}
+
+export default LinearGradient
